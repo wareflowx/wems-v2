3c13f0c9bea9dda8d8f19e7374fbfe164b5dabb9
rohit <rohitsoni259@gmail.com>
2025-11-09 18:20:50 +0530

    memory router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { RouterProvider } from '@tanstack/react-router';
 import './index.css';
-import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './components/ThemeProvider';
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { router } from './utils/routes';
 
 const queryClient = new QueryClient();
 
-// Create a new router instance
-const router = createRouter({ routeTree });
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
 
 function App() {
   return (
